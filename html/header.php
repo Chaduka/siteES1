98fc496bbb5ae2be24cfe57108ff247134312907
Chady Chaito <chadychaito@gmail.com>
2016-11-25 00:20:19 -0200

    Adicionado site

---
+++ html/header.php
@@ -0,0 +1,25 @@
+<header class="main-header">
+  <!-- Logo -->
+  <a href="index.php" class="logo">
+    <!-- mini logo for sidebar mini 50x50 pixels -->
+    <span class="logo-mini"><b>18</b></span>
+    <!-- logo for regular state and mobile devices -->
+    <span class="logo-lg"><b>Grupo</b>18</span>
+  </a>
+  <!-- Header Navbar: style can be found in header.less -->
+  <nav class="navbar navbar-static-top">
+    <!-- Sidebar toggle button-->
+    <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+      <span class="sr-only">Toggle navigation</span>
+    </a>
+
+    <div class="navbar-custom-menu">
+      <ul class="nav navbar-nav">
+        <!-- Messages: style can be found in dropdown.less-->
+        <li class="dropdown messages-menu">
+          <a href="#" data-toggle="control-sidebar"><i class="fa fa-close"></i></a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</header>
